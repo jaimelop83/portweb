704734d3b99ce13a9a5484a2f0e7126382a0c94d
jaimelop83 <jaime.a.lopez@gmail.com>
2023-06-13 11:26:39 -0400

    added project links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Projects = () => {
   const projectData = [
-    { title: "Project 1", description: "This is a brief description of Project 1" },
-    { title: "Project 2", description: "This is a brief description of Project 2" },
+    { title: "RITH Trading App", description: "This is an application designed to have trading capabilities and financial analysis. Used the PERN stack and AWS", link: "https://rith-trading.onrender.com/" },
+    { title: "Stocks App", description: "This is an application that is designed to provide a financial analysis when entering a stock ticker along with a few fundamental charts. Using openai, polygon.io and plotly", link: "https://main--bejewelled-malabi-d4c0e1.netlify.app/" },
     { title: "Project 3", description: "This is a brief description of Project 3" },
   ];
 
@@ -15,9 +15,11 @@ const Projects = () => {
             <div className="bg-white rounded-lg shadow-md p-6">
               <h2 className="text-xl font-bold">{project.title}</h2>
               <p className="text-gray-600">{project.description}</p>
+              < a href={project.link} target="_blank" rel="noopener noreferrer">
               <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 View Project
               </button>
+              </a>
             </div>
           </div>
         ))}
